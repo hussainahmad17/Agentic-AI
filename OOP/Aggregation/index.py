103f36af9bc19834728e2b8b2ec5acd467110c34
Hussain Ahmad <hussainahmad.dev.17@gmail.com>
2026-05-26 19:07:38 +0500

    Add Customer and Address classes with profile editing functionality

---
+++ OOP/Aggregation/index.py
@@ -0,0 +1,30 @@
+class Customer:
+
+    def __init__(self, name, gender, address):
+        self.name = name
+        self.gender = gender
+        self.address = address
+
+    
+    def edit_profile(self, new_name, new_city, new_state):
+        self.name = new_name
+        self.address.change_address(new_city, new_state)
+
+
+class Address:
+
+    def __init__(self, city, state):
+        self.city = city
+        self.state = state
+
+
+    def change_address(self, new_city, new_state):
+        self.city = new_city
+        self.state = new_state
+
+
+hussains_address = Address("Fasisalabad", "Punjab")
+hussain = Customer("Hussain", "Male", hussains_address)
+hussain.edit_profile("Hussain Ali", "Lahore", "Punjab")
+
+print(hussain.name) # Hussain Ali
